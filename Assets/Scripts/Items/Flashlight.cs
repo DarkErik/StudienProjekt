c194be054c4c3c71be6dd7594b48aa5341d79098
Johanna <schulte_johanna02@gmx.de>
2022-11-01 12:22:46 +0100

    Unfinished functionality of the flashlight

--- Assets/Scripts/Items/Flashlight.cs
+++ Assets/Scripts/Items/Flashlight.cs
@@ -6,22 +6,57 @@ public class Flashlight : MonoBehaviour
 {
     private bool isFlashlight = false;
     private float flash;
-    private bool isReady = true;
+    public bool isBright = false;
+    public GameObject[] scientists;
 
     void Start()
     {
-        
+        //get all scientists in this scene
+        scientists = GameObject.FindGameObjectsWithTag("Scientist");
     }
 
     void Update()
     {
+        //turn flashlight on and off by space
         flash = Input.GetAxis("Flashlight");
-        
         if (flash != 0)
         {
-            isFlashlight = !isFlashlight;
+            if (isFlashlight)
+            {
+                isBright = !isBright;
+                transform.GetChild(0).gameObject.SetActive(isBright);
+                isFlashlight = false;
+            }
+        }
+        if (flash == 0)
+        {
+            isFlashlight = true;
         }
 
-        transform.GetChild(0).gameObject.SetActive(isFlashlight);
+        //if the flashlight is on, set variable pickUp in the scienceguy script to true, if its off, set putDown to true
+        if(isBright)
+        {
+            if(scientists.Length != 0)
+            {
+                foreach(GameObject scienceGuy in scientists)
+                {
+                    PickFlashlight pick = (PickFlashlight) scienceGuy.GetComponent(typeof(PickFlashlight));
+                    pick.pickUp = true;
+                }
+            }
+        }
+        else
+        {
+            if (scientists.Length != 0)
+            {
+                foreach (GameObject scienceGuy in scientists)
+                {
+                    PickFlashlight pick = (PickFlashlight)scienceGuy.GetComponent(typeof(PickFlashlight));
+                    pick.putDown = true;
+                    transform.SetParent(null);
+                }
+            }
+        }
     }
+
 }
---
+++ Assets/Scripts/Items/PickFlashlight.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickFlashlight : MonoBehaviour
+{
+    public bool pickUp = false;
+    public bool putDown = false;
+    [SerializeField] private Animator animator;
+    private void Update()
+    {
+        if (putDown)
+        {
+            animator.SetBool("PutDown", true);
+            putDown = false;
+            animator.SetBool("Pick", false);
+        }
+    }
+
+
+    //if the flashlight is on, pick it up when scientist is touching it, then set scientist as a parent
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (pickUp)
+        {
+            if(collision.tag == "PowerUp")
+            {
+                animator.SetBool("Pick", true);
+                collision.gameObject.transform.SetParent(this.transform);
+            }
+            animator.SetBool("PutDown", false);
+            pickUp = false;
+        }
+    }
+}
