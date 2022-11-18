68222d48c3d684b01ab3edc44e4b12fef30c6f17
Julius Müther <julius.muether@ruhr-uni-bochum.de>
2022-11-18 14:03:38 +0100

    Scene Transition Shader
    added shader & material, added script, added separate Canvas

---
+++ Assets/Scripts/Util/SceneTransitionController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionController : MonoBehaviour
+{
+
+    public string sceneToLoad;
+    public float speed = 2f;
+    public Texture2D texture;
+
+    private Material material;
+    private bool shouldReveal;
+
+    void Start()
+    {
+        material = GetComponent<Image>().material;
+        material.SetTexture("_TransitionTex", texture);
+        shouldReveal = true;
+    }
+
+    void Update()
+    {
+        if (shouldReveal) //open animation
+        {
+            material.SetFloat("_Cutoff", Mathf.MoveTowards(material.GetFloat("_Cutoff"), 1.1f, speed * Time.deltaTime));
+        } else //close animation and load new scene
+        {
+            material.SetFloat("_Cutoff", Mathf.MoveTowards(material.GetFloat("_Cutoff"), -0.1f - material.GetFloat("_EdgeSmoothing"), speed * Time.deltaTime));
+            if (material.GetFloat("_Cutoff") == (-0.1f - material.GetFloat("_EdgeSmoothing")))
+                SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    public void ChangeScene()
+    {
+        shouldReveal = false;
+    }
+
+}
