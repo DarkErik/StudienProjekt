30c7c2085f6bd8c17bde2324fe62ab37961271c6
Erik Diers <erik.diers@rub.de>
2022-12-09 16:41:25 +0100

    Cleaned up project and enhanced morphing

--- Assets/Scripts/Player/Morph.cs
+++ Assets/Scripts/Player/Morph.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class Morph : MonoBehaviour
 {
-    private GameObject itemSlot1;
-    private GameObject itemSlot2;
+    //private GameObject itemSlot1;
+    //private GameObject itemSlot2;
 
-    private PowerUpObject item1;
-    private PowerUpObject item2;
-    private Image item1Image;
-    private Image item2Image;
+    //private PowerUpObject item1;
+    //private PowerUpObject item2;
+    //private Image item1Image;
+    //private Image item2Image;
 
     private float timer;
     private bool canMorph;
@@ -23,47 +23,47 @@ public class Morph : MonoBehaviour
         canMorph = false;
         inCollider = false;
 
-        itemSlot1 = GameObject.Find("/MainCanvas/ItemOverlay/Item1Background/Item1");
-        itemSlot2 = GameObject.Find("/MainCanvas/ItemOverlay/Item2Background/Item2");
+        //itemSlot1 = GameObject.Find("/MainCanvas/ItemOverlay/Item1Background/Item1");
+        //itemSlot2 = GameObject.Find("/MainCanvas/ItemOverlay/Item2Background/Item2");
 
-        if (itemSlot1 != null)
-        {
-            item1 = itemSlot1.GetComponent<PowerUpObject>();
-            item1Image = itemSlot1.GetComponent<Image>();
-        }
+        //if (itemSlot1 != null)
+        //{
+        //    item1 = itemSlot1.GetComponent<PowerUpObject>();
+        //    item1Image = itemSlot1.GetComponent<Image>();
+        //}
 
-        if (itemSlot2 != null)
-        {
-            item2 = itemSlot2.GetComponent<PowerUpObject>();
-            item2Image = itemSlot2.GetComponent<Image>();
-        }
+        //if (itemSlot2 != null)
+        //{
+        //    item2 = itemSlot2.GetComponent<PowerUpObject>();
+        //    item2Image = itemSlot2.GetComponent<Image>();
+        //}
         
     }
 
     void Update()
     {
-        if (itemSlot1 == null)
-        {
-            itemSlot1 = GameObject.Find("/Manager/MainCanvas/ItemOverlay/Item1");
-            item1 = itemSlot1.GetComponent<PowerUpObject>();
-            item1Image = itemSlot1.GetComponent<Image>();
-        }
+        //if (itemSlot1 == null)
+        //{
+        //    itemSlot1 = GameObject.Find("/Manager/MainCanvas/ItemOverlay/Item1");
+        //    item1 = itemSlot1.GetComponent<PowerUpObject>();
+        //    item1Image = itemSlot1.GetComponent<Image>();
+        //}
 
-        if (itemSlot2 == null)
-        {
-            itemSlot2 = GameObject.Find("/Manager/MainCanvas/ItemOverlay/Item2");
-            item2 = itemSlot2.GetComponent<PowerUpObject>();
-            item2Image = itemSlot2.GetComponent<Image>();
-        }
+        //if (itemSlot2 == null)
+        //{
+        //    itemSlot2 = GameObject.Find("/Manager/MainCanvas/ItemOverlay/Item2");
+        //    item2 = itemSlot2.GetComponent<PowerUpObject>();
+        //    item2Image = itemSlot2.GetComponent<Image>();
+        //}
 
-        if (itemSlot1 != null && itemSlot2 != null)
-        {
+        //if (itemSlot1 != null && itemSlot2 != null)
+        //{
             if (!canMorph)
                 canMorph = 0.5f < (timer += Time.deltaTime);
 
             if (!inCollider)
                 CheckMorph();
-        }
+        //}
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -76,18 +76,18 @@ public class Morph : MonoBehaviour
                 if (Input.GetAxis("Morph1") > 0)
                 {
                     GameObject newPlayer = collision.gameObject.GetComponent<PowerUpObject>().PlayerObject;
-                    item1.PlayerObject = newPlayer;
-                    item1Image.sprite = collision.gameObject.GetComponent<SpriteRenderer>().sprite;
-                    item1Image.SetNativeSize();
+                    ItemOverlaySingelton.Instance.powerup1.PlayerObject = newPlayer;
+                    ItemOverlaySingelton.Instance.item1.sprite = collision.gameObject.GetComponent<SpriteRenderer>().sprite;
+                    ItemOverlaySingelton.Instance.item1.SetNativeSize();
                     Morphing(newPlayer);
                 }
                 
                 if (Input.GetAxis("Morph2") > 0)
                 {
                     GameObject newPlayer = collision.gameObject.GetComponent<PowerUpObject>().PlayerObject;
-                    item2.PlayerObject = newPlayer;
-                    item2Image.sprite = collision.gameObject.GetComponent<SpriteRenderer>().sprite;
-                    item2Image.SetNativeSize();
+                    ItemOverlaySingelton.Instance.powerup2.PlayerObject = newPlayer;
+                    ItemOverlaySingelton.Instance.item2.sprite = collision.gameObject.GetComponent<SpriteRenderer>().sprite;
+                    ItemOverlaySingelton.Instance.item2.SetNativeSize();
                     Morphing(newPlayer);
                 }
             }
@@ -106,14 +106,14 @@ public class Morph : MonoBehaviour
         if (canMorph) {
                 if (Input.GetAxis("Morph1") > 0)
                 {
-                    if (item1.PlayerObject != null)
-                        Morphing(item1.PlayerObject);
+                    if (ItemOverlaySingelton.Instance.powerup1.PlayerObject != null)
+                        Morphing(ItemOverlaySingelton.Instance.powerup1.PlayerObject);
                 }
                 
                 if (Input.GetAxis("Morph2") > 0)
                 {
-                    if (item2.PlayerObject != null)
-                        Morphing(item2.PlayerObject);
+                    if (ItemOverlaySingelton.Instance.powerup2.PlayerObject != null)
+                        Morphing(ItemOverlaySingelton.Instance.powerup2.PlayerObject);
                 }
             }
     }
@@ -197,16 +197,9 @@ public class Morph : MonoBehaviour
 
     public static void RemoveAllItemsAndSlots()
     {
-        GameObject itemSlot1 = GameObject.Find("Manager/MainCanvas/ItemOverlay/Item1");
-        GameObject itemSlot2 = GameObject.Find("Manager/MainCanvas/ItemOverlay/Item2");
-        PowerUpObject item1 = itemSlot1.GetComponent<PowerUpObject>();
-        PowerUpObject item2 = itemSlot2.GetComponent<PowerUpObject>();
-        Image item1Image = itemSlot1.GetComponent<Image>();
-        Image item2Image = itemSlot2.GetComponent<Image>();
-
-        item1.PlayerObject = null;
-        item2.PlayerObject = null;
-        item1Image.sprite = null;
-        item2Image.sprite = null;
+        ItemOverlaySingelton.Instance.powerup1.PlayerObject = null;
+        ItemOverlaySingelton.Instance.powerup2.PlayerObject = null;
+        ItemOverlaySingelton.Instance.item1.sprite = null;
+        ItemOverlaySingelton.Instance.item2.sprite = null;
     }
 }
