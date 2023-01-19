44ab3d2eac23c4da07d4fa6b9b836bc3c28ed35d
Erik Diers <erik.diers@rub.de>
2023-01-19 14:45:25 +0100

    Minor fixes

--- Assets/Scripts/UI_Scripts/MainMenu.cs
+++ Assets/Scripts/UI_Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Start");
     }
 
     public void QuitGame()
